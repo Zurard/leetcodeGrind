1b2dc4601e1e752b25a4e8bfb0505172a7933202
Shreetej Meshram <zurard07@gmail.com>
2026-08-07 12:03:39 +0530

    Time: 59 ms (5.18%) | Memory: 73.4 MB (5.00%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        from functools import cache 
+        @cache 
+
+        def solve(day , can_buy):
+            if day == len(prices):
+                return 0
+            if can_buy : 
+                buy = -prices[day] + solve(day+1 , False)
+                skip = solve(day+1 , can_buy)
+                return max(buy,skip)
+            if not can_buy:
+                sell = prices[day] + solve(day+1 , True)  
+                hold = solve(day+1 , can_buy)
+                return max(sell, hold)
+            
+        return solve(0,True)
+
+
+        
