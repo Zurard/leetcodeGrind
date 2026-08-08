a3762c919a7fd41030ed59cad601712d670b0f9d
Shreetej Meshram <zurard07@gmail.com>
2026-08-09 01:15:52 +0530

    Time: 141 ms (60.08%) | Memory: 15.5 MB (29.40%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        # this is a sliding window problem where we grow and shrink the window based on how the charecter are matching 
+
+        left = 0
+        freq = {}  
+        max_freq = 0
+        ans = 0 
+
+        for right in range(len(s)): 
+            freq[s[right]] = freq.get(s[right], 0) + 1
+            max_freq = max(max_freq, freq[s[right]] )
+
+            while (right - left + 1) - max_freq > k :
+                freq[s[left]] -= 1
+                left+=1
+
+            ans = max(ans , right - left + 1)            
+        
+        return ans
