ca53f9d14c8320e84617039a5f704353b5347122
Shreetej Meshram <zurard07@gmail.com>
2026-08-26 14:34:20 +0530

    Time: 143 ms (56.13%) | Memory: 70.7 MB (17.98%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.py
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.py
@@ -1,10 +1,12 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
         n = len(edges)
-        dist1 = [-1] * n
+        dist1 = [-1] * n 
+        dist2 = [-1] * n
+
         dist1[node1] = 0  
-        dist2 = [-1] * n 
         dist2[node2] = 0 
+        
         def dfs(u , currDistance ,arrDistance ):
             v = edges[u]
             if v == -1 or arrDistance[v] != -1 :
@@ -17,9 +19,6 @@ class Solution:
         dfs(node1, 0 , dist1)
         dfs(node2, 0 , dist2)
         
-        # print(dist1)
-        # print(dist2)
-
         ans = - 1 
         best= float("inf") 
 
